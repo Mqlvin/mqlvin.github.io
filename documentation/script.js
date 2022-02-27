e427c3aed04c0a1716bc50d48312491dff6f51ff
Mqlvin <pricklydotcactus@gmail.com>
2022-02-27 17:46:25 +0000

    Added documentation template.

---
+++ documentation/script.js
@@ -0,0 +1,52 @@
+
+
+function addEventListeners() {
+    var pagesList = document.getElementsByClassName("page-list-item");
+    var element;
+    for(element = 0; element < pagesList.length; element++) {
+        pagesList[element].addEventListener("click", function(event) {
+            var iter;
+            for(iter = 0; iter < pagesList.length; iter++) {
+                pagesList[iter].classList.remove("active-item");
+            }
+            event.target.classList.add("active-item");
+        });
+    }
+
+
+    var list = document.getElementsByClassName("chapter-expander");
+    var element;
+    for(element = 0; element < list.length; element++) {
+        list[element].addEventListener("click", function(event) {
+            event.target.closest(".chapter-parent").getElementsByClassName("chapter-title")[0].getElementsByClassName("chapter-expander")[0].classList.toggle("chapter-expander-active");
+            event.target.closest(".chapter-parent").getElementsByClassName("chapter-title")[0].getElementsByClassName("chapter-text")[0].classList.toggle("chapter-text-active");
+            var content = (event.target.closest(".chapter-parent").getElementsByClassName("expandable-list"))[0];
+            if(content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+
+    var textList = document.getElementsByClassName("chapter-text");
+    var element;
+    for(element = 0; element < textList.length; element++) {
+        textList[element].addEventListener("click", function(event) {
+            event.target.closest(".chapter-parent").getElementsByClassName("chapter-title")[0].getElementsByClassName("chapter-expander")[0].classList.toggle("chapter-expander-active");
+            event.target.closest(".chapter-parent").getElementsByClassName("chapter-title")[0].getElementsByClassName("chapter-text")[0].classList.toggle("chapter-text-active");
+            var content = (event.target.closest(".chapter-parent").getElementsByClassName("expandable-list"))[0];
+            if(content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+}
+
+function hoverBGC(color, element) {
+    element.closest(".chapter-parent").style.backgroundColor = color;
+}
+
+
