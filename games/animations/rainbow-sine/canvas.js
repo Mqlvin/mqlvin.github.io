ef086e7f600f98d3cef0cdcc53608a6ad306e9e2
Mqlvin <pricklydotcactus@gmail.com>
2022-04-08 08:00:14 +0100

    Add animations folder, add sine animation

---
+++ games/animations/rainbow-sine/canvas.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector(".matrix-canvas");
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let current = 0.0;
+let rainbow = true;
+let lineCount = 20;
+let rainbowOffset = 0.0;
+let amplifier = 200;
+let scale = 12;
+
+class Line {
+    constructor(offset) {
+        this.offset = offset;
+        console.log(offset);
+        this.x = canvas.width / 2 + ((offset - lineCount / 2) * (100 - lineCount)) + 12
+
+        this.renderx = this.x + (this.offset);
+        this.rendery = canvas.height / 2;
+        this.width = 10;
+    }
+
+    draw() {
+        c.strokeStyle = (rainbow ? "hsl(" + (360 * (this.offset - rainbowOffset) / lineCount) + ", 80%, 60%)" : "rgb(255, 255, 255)");
+        c.lineWidth = 15;
+        c.strokeRect(this.renderx, this.rendery, this.width, Math.sin(current - 1 / lineCount * this.offset * scale) * amplifier);
+        c.strokeRect(this.renderx, this.rendery, this.width, -Math.sin(current - 1 / lineCount * this.offset * scale) * amplifier);
+    }
+}
+
+let lines = [];
+for(var i = 0; i < lineCount; i++) {
+    lines.push(new Line(i));
+}
+
+tick();
+
+function tick() {
+    requestAnimationFrame(tick);
+    c.fillStyle = "rgba(16, 16, 16, 0.6)";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+    lines.forEach((line, index) => {
+        line.draw();
+    });
+    current += 0.06;
+    rainbowOffset += 0.2;
+}
