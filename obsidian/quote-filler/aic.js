f8eeb9a8644a2a0d53784efa3bcbcb699b167fe9
Mqlvin <79374369+Mqlvin@users.noreply.github.com>
2024-03-29 10:20:14 +0000

    Make AIC harder

--- obsidian/quote-filler/aic.js
+++ obsidian/quote-filler/aic.js
@@ -1,5 +1,5 @@
-const WORDS_SHOWN = 5; // max words shown
-const REQUIRE_MINIMUM = 2; // require at least this many words
+const WORDS_SHOWN = 3; // max words shown
+const REQUIRE_MINIMUM = 4; // require at least this many words
 
 function appendQuote(parent, text) {
     let quoteWords = text.split(" ");
